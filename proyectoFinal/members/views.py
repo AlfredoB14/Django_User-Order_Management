8d48257784643f7cda368566d578f2d204135158
AlfredoB14 <alfredobarranco14@gmail.com>
2024-05-01 17:27:10 -0600

    Añadido: Base.html, archivo que se extiende a todos los HTML para enseñar siempre su contenido, en este caso para no copiar todo el codigo de la navbar en cada HTML y también para el Bootstrap.
    Añadido el Login

--- proyectoFinal/members/views.py
+++ proyectoFinal/members/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def login_user(request):
+
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            messages.success(request, ("Successfully logged in"))
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, ("Error trying to log in"))
+            return redirect('log')
+
+
+    else:
+        return render(request, 'authenticate/login.html', {})
