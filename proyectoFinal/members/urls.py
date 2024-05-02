5af40a82a0a8c1e66f3edb2bd9048befdc43d7ab
AlfredoB14 <alfredobarranco14@gmail.com>
2024-05-01 19:26:54 -0600

    Registro de Usuarios Completado

--- proyectoFinal/members/urls.py
+++ proyectoFinal/members/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('login_user/', views.login_user, name="log"),
+    path('logout_user/', views.logout_user, name="logt"),
+    path('register_user/', views.register_user, name="register")
 ]
