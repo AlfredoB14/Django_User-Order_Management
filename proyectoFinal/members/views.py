d0953c7604e45c27802cd942cdf169fedb1792e4
AlfredoB14 <alfredobarranco14@gmail.com>
2024-05-06 14:54:49 -0600

    Modify User

--- proyectoFinal/members/views.py
+++ proyectoFinal/members/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .forms import RegisterUserForm, ModifyUserForm
 
 def login_user(request):
 
@@ -42,7 +43,7 @@ def register_user(request):
 
     if request.method == "POST":
 
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
 
         if form.is_valid():
 
@@ -56,14 +57,14 @@ def register_user(request):
             return redirect('home')
         
         else:
-            form = UserCreationForm()
+            form = RegisterUserForm()
             messages.success(request, ("Error with register"))
             return render(request, 'authenticate/register_user.html', {
                 'form':form,
             })
 
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
         return render(request, 'authenticate/register_user.html', {
             'form':form,
         })
@@ -90,7 +91,10 @@ def delete_user(request, userid):
 
             messages.success(request, ("The user was Deleted Successfully"))
             user.delete()
-            logout(request)
+
+            if not request.user.is_staff:
+                logout(request)
+
             return redirect('home')
         
         else:
@@ -102,4 +106,24 @@ def delete_user(request, userid):
         return redirect('home')
 
 def modify_user(request, userid):
-    pass
+
+    if request.user.is_authenticated:
+
+        user =  User.objects.get(pk=userid)
+
+        if user.id == request.user.id or request.user.is_staff:
+            
+            form = ModifyUserForm(request.POST or None, instance=user)
+
+            if form.is_valid():
+                form.save()
+                return redirect('manage')
+
+            return render(request, 'authenticate/modifyUser.html', {'user':user, 'form':form})
+        
+        else:
+            messages.success(request, ("You don't have permission to do that!"))
+            return redirect('home')   
+    else:
+        messages.success(request, ("You are not logged in"))
+        return redirect('home')
