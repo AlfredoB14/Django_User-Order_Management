8350b27722b5a0c5c41b4f652b526063de13dc95
AlfredoB14 <alfredobarranco14@gmail.com>
2024-05-09 16:27:47 -0600

    Cambios en Settings

--- proyectoFinal/proyectoFinal/settings.py
+++ proyectoFinal/proyectoFinal/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'events',
     'members',
     'batch',
+    'proyectofinal',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
